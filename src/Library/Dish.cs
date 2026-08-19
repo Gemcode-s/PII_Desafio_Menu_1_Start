bd43c6bd1d795f7f69926970bc9d94561bae57cc
bruno <broofafa@gmail.com>
2026-08-19 08:55:27 -0300

    Subida de dish.cs

--- src/Library/Dish.cs
+++ src/Library/Dish.cs
@@ -6,10 +6,17 @@
 
 namespace Ucu.Poo.Restaurant
 {
-    /// <summary>
-    /// Representa un platillo individual en el menú del restaurante.
-    /// </summary>
+    
     public class Dish
     {
+        public string Name{get;set ;}
+        public double Price{get;set;}
+        public bool IsVegetarian{get;set;}
+        public Dish(string name, double price, bool isVegetarian)
+        {
+            Name=name;
+            Price=price;
+            IsVegetarian=isVegetarian;
+        }
     }
 }
